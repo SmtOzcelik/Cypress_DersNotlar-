9a796514251e9e6d756cac67f778b70be5414c63
SmtOzcelik <smtab142790@gmail.com>
2023-10-05 16:59:21 +0300

    04/10/2023

---
+++ cypress/e2e/day05/01_DragAndDorp.cy.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+describe('Drog and Drop',()=>{
+
+    it('',()=>{
+    
+        cy.visit('https://the-internet.herokuapp.com/drag_and_drop')
+        
+    
+    })
+    
+    it('',()=>{
+    
+        
+    })
+    
+    
+    it('',()=>{
+        
+            
+    })
+    
+    
+    
+    })
