c66f20e0842e3a8cd57f50baa47c8d5315c4c86e
SmtOzcelik <smtab142790@gmail.com>
2023-10-08 12:28:55 +0300

    05/10/2023

---
+++ cypress/e2e/day09/02_Simple_API_Test.cy.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+describe('Simple API Test',()=>{
+    const requestURL='https://jsonplaceholder.cypress.io/comments'
+
+    it('Header Assert 1',()=>{
+        cy.request(requestURL).its('headers').its('')
+
+
+    
+    
+    })
+    
+    it('Header Assert 2',()=>{
+    
+        
+    })
+    
+    
+    
+    
+    
+    })
