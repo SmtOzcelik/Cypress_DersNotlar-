26e1decd8f8c75114c93baf60364cdbae677db70
SmtOzcelik <smtab142790@gmail.com>
2023-10-09 10:38:39 +0300

    05/10/2023

---
+++ cypress/e2e/day10/01_POST_With _Auth.cy.js
@@ -0,0 +1,43 @@
+/// <reference types="cypress" />
+
+describe('POST with Auth',()=>{
+
+    it('Test',()=>{
+        
+        cy.request({
+            url:"https://restful-booker.herokuapp.com/booking",
+            metod:'POST',
+            auth:{
+                "username" : "admin",
+                "password" : "password123"
+            },
+            body:{
+                "firstname": "Samet",
+                "lastname": "Tsl",
+                "totalprice": 111,
+                "depositpaid": true,
+                "bookingdates": {
+                    "checkin": "2014-10-10",
+                    "checkout": "2014-11-10"
+                },
+                "additionalneeds": "Breakfast",
+                "token": "abc123"
+            }
+        }).then((response)=>{
+            console.log(response)
+
+            expect(response.status).to.equal(200)
+            expect(response.body.booking.firstname).to.eq("Samet")
+            expect(response.body.booking.lastname).to.eq("Tsl")
+            expect(response.body.booking.totalprice).to.eq(111)
+            expect(response.body.bookingdates.checkin).to.eq("2014-10-10")
+
+        })
+    
+    
+    })
+    
+    
+    
+    
+    })
